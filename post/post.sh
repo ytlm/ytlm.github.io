cb4b2719ffb6e73a530b3ce3e4a430a33463e388
ytlm <ytlm1314@gmail.com>
2017-12-05 11:13:41 +0800

    update posh.sh and add favicon

--- post/post.sh
+++ post/post.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-pushd ../public/
-
-cat sitemap.xml | grep -P "https?://ytlm[^<>]*" -o  > urls.txt
+cat ./public/sitemap.xml | grep -P "https?://ytlm[^<>]*" -o  > urls.txt
 
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://ytlm.github.io&token=6IHH93TjHvvymgnY"
 
-popd
+rm -rf urls.txt
