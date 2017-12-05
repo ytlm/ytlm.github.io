1fc5af5d581e306d38284d63d5844ffa22d4398d
Tinglong Yang <tinglong.yang@sihuatech.com>
2017-12-05 10:43:05 +0800

    add ariticle about awk

--- post/post.sh
+++ post/post.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-cd public/
+pushd ../public/
 
 cat sitemap.xml | grep -P "https?://ytlm[^<>]*" -o  > urls.txt
 
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://ytlm.github.io&token=6IHH93TjHvvymgnY"
 
-cd ..
+popd
