4ce28dfb3ca230bfea9cbc8b6440fdffbdf70c59
ytlm <ytlm1314@gmail.com>
2017-12-05 16:52:10 +0800

    update https

--- post/post.sh
+++ post/post.sh
@@ -2,6 +2,6 @@
 
 cat ./public/sitemap.xml | grep -P "https?://ytlm[^<>]*" -o  > urls.txt
 
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://ytlm.github.io&token=6IHH93TjHvvymgnY"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://ytlm.github.io&token=6IHH93TjHvvymgnY"
 
 rm -rf urls.txt
